f645328ef136a58e781523ab856856b196ffe9e6
Adriana <adrianapardo1998@gmail.com>
2023-10-17 22:13:17 +0200

    added NewNoteActivity

--- app/src/main/java/com/example/menotes/MyNotesActivity.kt
+++ app/src/main/java/com/example/menotes/MyNotesActivity.kt
@@ -1,12 +1,20 @@
 package com.example.menotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MyNotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_notes)
 
+        val addButton = findViewById<FloatingActionButton>(R.id.add_btt)
+
+        addButton.setOnClickListener {
+            val intent = Intent(this, NewNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/menotes/NewNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.menotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_note)
+
+    }
+}
