5c5fd737728e947464f2ac1e3a15e5f0cdf84834
Adriana <adrianapardo1998@gmail.com>
2023-10-24 23:55:40 +0200

    added add note functionalities

--- app/src/main/java/com/example/menotes/MyNotesActivity.kt
+++ app/src/main/java/com/example/menotes/MyNotesActivity.kt
@@ -1,44 +1,65 @@
 package com.example.menotes
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MyNotesActivity : AppCompatActivity() {
 
-    private val notes = mutableListOf(
-        Note("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin orci lacus, vestibulum non dictum ut, tempus et nibh. Aenean sollicitudin sapien sit amet leo tempus, quis volutpat leo pellentesque. Morbi ut ligula ac nisi condimentum scelerisque. Donec id ligula turpis. Fusce tristique euismod bibendum. Suspendisse eu consequat mauris, in scelerisque elit. Nam quis aliquet erat, ac molestie odio. Curabitur rhoncus augue eros, nec finibus enim accumsan nec. Donec dolor enim, viverra in pharetra a, imperdiet id nunc. Etiam lacinia odio mollis ultricies finibus. In consequat quam ac tortor sollicitudin interdum. Vestibulum gravida lacus ac odio posuere, at aliquet mauris ullamcorper."),
-        Note("Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
-        Note("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin orci lacus, vestibulum non dictum ut, tempus et nibh.")
-    )
+    private val notes = mutableListOf<Note>()
 
     private lateinit var rvNotes: RecyclerView
     private lateinit var notesAdapter: NotesAdapter
+
+    private lateinit var addBtt: FloatingActionButton
+
+    private lateinit var startForResult: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_notes)
         initComponent()
         initUI()
+        initListeners()
 
-        val addButton = findViewById<FloatingActionButton>(R.id.add_btt)
+        startForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+                    val newNote = data?.getStringExtra("EXTRA_NAME_NOTE")
+                    newNote?.let {
+                        notes.add(Note(it))
+                        updateNotes()
+                    }
+                }
+            }
+    }
 
-        addButton.setOnClickListener {
+    private fun initListeners() {
+        addBtt.setOnClickListener {
             val intent = Intent(this, NewNoteActivity::class.java)
-            startActivity(intent)
+            startForResult.launch(intent)
         }
     }
 
     private fun initComponent() {
         rvNotes = findViewById(R.id.rvNotes)
+        addBtt = findViewById(R.id.add_btt)
     }
 
     private fun initUI() {
         notesAdapter = NotesAdapter(notes)
         rvNotes.layoutManager = LinearLayoutManager(this)
         rvNotes.adapter = notesAdapter
+    }
+
+    private fun updateNotes() {
+        notesAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/menotes/NewNoteActivity.kt
+++ app/src/main/java/com/example/menotes/NewNoteActivity.kt
@@ -1,5 +1,6 @@
 package com.example.menotes
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,24 +8,31 @@ import android.widget.EditText
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NewNoteActivity : AppCompatActivity() {
+
     private lateinit var editTextNote: EditText
+    private lateinit var saveBtt: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_note)
+        initComponent()
+        initListeners()
+    }
 
-        editTextNote = findViewById(R.id.etNote)
-
-        val saveButton = findViewById<FloatingActionButton>(R.id.save_btt)
-
-        saveButton.setOnClickListener {
+    private fun initListeners() {
+        saveBtt.setOnClickListener {
             val note = editTextNote.text.toString()
-
             if (note.isNotEmpty()) {
                 val intent = Intent(this, MyNotesActivity::class.java)
-
-                startActivity(intent)
+                intent.putExtra("EXTRA_NAME_NOTE", note)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
             }
         }
+    }
+
+    private fun initComponent() {
+        saveBtt = findViewById(R.id.save_btt)
+        editTextNote = findViewById(R.id.etNote)
     }
 }
